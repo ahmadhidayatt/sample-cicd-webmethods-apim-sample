8de5a6312c3af0b65f987fc43b3efde41fb53ce9
ahmad <ahmadHidayat@gmail.com>
2026-04-20 18:00:10 +0700

    update config

--- apigw-jte-library/libraries/apigw/steps/prepareScripts.groovy
+++ apigw-jte-library/libraries/apigw/steps/prepareScripts.groovy
@@ -11,9 +11,6 @@ void call() {
   writeFile file: 'gateway_utils.sh',
     text: libraryResource('bin/gateway_import_export_utils.sh')
 
-  writeFile file: 'gateway_setup.sh',
-    text: libraryResource('bin/gateway_setup.sh')
-
   sh '''
     chmod +x *.sh
   '''
