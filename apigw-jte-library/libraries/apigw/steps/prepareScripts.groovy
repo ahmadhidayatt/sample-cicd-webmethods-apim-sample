c8c5cdc6ce9bd84670e065b422bf02bef568c96c
ahmad <ahmadHidayat@gmail.com>
2026-04-20 17:10:40 +0700

    update config

--- apigw-jte-library/libraries/apigw/steps/prepareScripts.groovy
+++ apigw-jte-library/libraries/apigw/steps/prepareScripts.groovy
@@ -6,13 +6,13 @@ void call() {
   echo 'Preparing shell scripts from JTE library...'
 
   writeFile file: 'common.lib',
-    text: libraryResource('resources/bin/common.lib')
+    text: libraryResource('bin/common.lib')
 
   writeFile file: 'gateway_utils.sh',
-    text: libraryResource('resources/bin/gateway_import_export_utils.sh')
+    text: libraryResource('bin/gateway_import_export_utils.sh')
 
   writeFile file: 'gateway_setup.sh',
-    text: libraryResource('resources/bin/gateway_setup.sh')
+    text: libraryResource('bin/gateway_setup.sh')
 
   sh '''
     chmod +x *.sh
