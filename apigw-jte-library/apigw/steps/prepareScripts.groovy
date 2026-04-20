e9a652fdbff24865e7043360a148174e91dd7af8
ahmad <ahmadHidayat@gmail.com>
2026-04-21 00:28:50 +0700

    update config

--- apigw-jte-library/apigw/steps/prepareScripts.groovy
+++ apigw-jte-library/apigw/steps/prepareScripts.groovy
@@ -3,7 +3,7 @@ void call() {
   sh 'ls -R'
   echo 'DEBUG LIBRARY RESOURCE'
   sh 'ls -R $JENKINS_HOME/workspace/@libs || true'
-  def commonLib = libraryResource('common.lib')
+  String  commonLib = resource('common.lib')
 
   echo 'Preparing shell scripts from JTE library...'
 
