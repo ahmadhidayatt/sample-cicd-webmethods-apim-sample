5a496250f68e8d17f38e4742132461dfca12d6b6
ahmad <ahmadHidayat@gmail.com>
2026-04-21 01:20:37 +0700

    update config

--- apigw-jte-library/apigw/steps/importApi.groovy
+++ apigw-jte-library/apigw/steps/importApi.groovy
@@ -1,7 +1,6 @@
 void call(String gw) {
-
   sh """
-    source common.lib
-    import_api "${env.API_PROJECT}" "${gw}" "${env.APIGW_CREDS_USR}" "${env.APIGW_CREDS_PSW}"
+    chmod +x common.sh
+    ./common.shimport_api "${env.API_PROJECT}" "${gw}" "${env.APIGW_CREDS_USR}" "${env.APIGW_CREDS_PSW}"
   """
 }
--- apigw-jte-library/apigw/steps/rollback.groovy
+++ apigw-jte-library/apigw/steps/rollback.groovy
@@ -1,7 +1,6 @@
 void call(String gw) {
-
   sh """
-    source common.lib
-    rollback_api "${env.BACKUP_FILE}" "${gw}" "${env.APIGW_CREDS_USR}" "${env.APIGW_CREDS_PSW}"
+    chmod +x common.sh
+    ./common.shrollback_api "${env.BACKUP_FILE}" "${gw}" "${env.APIGW_CREDS_USR}" "${env.APIGW_CREDS_PSW}"
   """
 }
