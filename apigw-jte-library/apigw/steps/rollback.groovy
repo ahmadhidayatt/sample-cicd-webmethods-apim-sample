aa6134fb77e1eef97cf3b684e25215fdf4cc3a61
ahmad <ahmadHidayat@gmail.com>
2026-04-21 01:30:42 +0700

    update config

--- apigw-jte-library/apigw/steps/rollback.groovy
+++ apigw-jte-library/apigw/steps/rollback.groovy
@@ -1,6 +1,6 @@
 void call(String gw) {
   sh """
     chmod +x common.sh
-    ./common.shrollback_api "${env.BACKUP_FILE}" "${gw}" "${env.APIGW_CREDS_USR}" "${env.APIGW_CREDS_PSW}"
+    ./common.sh rollback_api "${env.BACKUP_FILE}" "${gw}" "${env.APIGW_CREDS_USR}" "${env.APIGW_CREDS_PSW}"
   """
 }
