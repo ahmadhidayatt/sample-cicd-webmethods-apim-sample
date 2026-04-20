cb2d4947e7e0cc2a77a3e4b4db2035769a285c6d
ahmad <ahmadHidayat@gmail.com>
2026-04-21 01:02:46 +0700

    update config

--- apigw-jte-library/apigw/steps/deploySequential.groovy
+++ apigw-jte-library/apigw/steps/deploySequential.groovy
@@ -1,12 +1,12 @@
 void call() {
   prepareScripts()
-  def gateways = config.apigw.gatewayUrls.split(',')
-  def esList   = config.apigw.esUrls.split(',')
-  def user     = config.apigw.username
-  def pass     = config.apigw.password
-  def apiName  = config.apigw.apiProject
+  def gateways = config.libraries.apigw.gatewayUrls.split(',')
+  def esList   = config.libraries.apigw.esUrls.split(',')
+  def user     = config.libraries.apigw.apigw.username
+  def pass     = config.libraries.apigw.apigw.password
+  def apiName  = config.libraries.apigw.apiProject
   
-  def apiProject = config.apigw.apiProject
+  def apiProject = config.libraries.apigw.apiProject
   def results  = [:]
 
   for (int i = 0; i < gateways.size(); i++) {
