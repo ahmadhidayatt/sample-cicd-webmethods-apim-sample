99c17b42a00767c4ca010dd13665c1ac2b585c63
ahmad <ahmadHidayat@gmail.com>
2026-04-20 22:38:29 +0700

    update config

--- apigw-jte-library/pipeline_template.groovy
+++ apigw-jte-library/pipeline_template.groovy
@@ -1,29 +1,3 @@
-pipeline {
-  agent any
-
-  options {
-    timestamps()
-    timeout(time: 90, unit: 'MINUTES')
-  }
-
-  stages {
-
-    stage('Init') {
-      steps {
-        loadProperties()
-      }
-    }
-
-    stage('Deploy') {
-      steps {
-        deploy()
-      }
-    }
-
-    stage('Summary') {
-      steps {
-        summary()
-      }
-    }
-  }
-}
+loadProperties()
+deploy()
+summary()
