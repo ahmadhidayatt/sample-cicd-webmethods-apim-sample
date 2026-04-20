a1163988ecae28f491129ccd0a9b11878c3934fe
ahmad <ahmadHidayat@gmail.com>
2026-04-20 22:28:24 +0700

    update config

--- apigw-jte-library/apigw/steps/prepareScripts.groovy
+++ apigw-jte-library/apigw/steps/prepareScripts.groovy
@@ -6,12 +6,12 @@ void call() {
  
 
 
-  def commonLib = libraryResource('apigw/resources/bin/common.lib')
+  def commonLib = libraryResource('bin/common.lib')
   if (!commonLib) {
     error 'common.lib not found in Shared Library resources/bin/'
   }
   echo 'Preparing shell scripts from JTE library...'
 
   writeFile file: 'common.lib',
-    text: libraryResource('apigw/resources/bin/common.lib')
+    text: libraryResource('bin/common.lib')
 }
