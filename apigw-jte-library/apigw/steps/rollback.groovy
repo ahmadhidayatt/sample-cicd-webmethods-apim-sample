54ab945c340d9d77d8909b5be81b61a8b1d5a8a7
ahmad <ahmadHidayat@gmail.com>
2026-04-21 09:54:05 +0700

    update config

--- apigw-jte-library/apigw/steps/rollback.groovy
+++ apigw-jte-library/apigw/steps/rollback.groovy
@@ -1,6 +1,14 @@
 void call(String gw) {
-  sh """
-    chmod +x common.sh
-    ./common.sh rollback_api "${env.BACKUP_FILE}" "${gw}" "${env.APIGW_CREDS_USR}" "${env.APIGW_CREDS_PSW}"
-  """
+
+  withCredentials([usernamePassword(
+    credentialsId: 'apigwcredential',
+    usernameVariable: 'U',
+    passwordVariable: 'P'
+  )]) {
+
+    sh """
+      chmod +x common.sh
+      ./common.sh rollback_api "${env.BACKUP_FILE}" "${gw}" "$U" "$P"
+    """
+  }
 }
