66cb2e26279efb2a2d2e406da9bbd5d200ea10ee
ahmad <ahmadHidayat@gmail.com>
2026-04-21 11:44:15 +0700

    update config

--- apigw-jte-library/apigw/steps/loadProperties.groovy
+++ apigw-jte-library/apigw/steps/loadProperties.groovy
@@ -1,7 +1,9 @@
 void call() {
+  env.REPO_URL       = config.repoUrl
+  env.REPO_BRANCH    = config.repoBranch
+  env.REPO_CRED_ID   = config.repoCredId
 
   def props = readProperties file: 'apigw-pipeline/jenkins/jenkins.properties'
-
   env.API_PROJECT        = props['api_project']
   env.APIGATEWAY_URLS    = props['apigateway_urls']
   env.APIGATEWAY_ES_URLS = props['apigateway_es_urls']
