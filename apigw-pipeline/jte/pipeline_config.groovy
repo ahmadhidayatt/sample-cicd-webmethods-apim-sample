547b1a51cb2014ce2dd34a77c29e00e85789fbb7
ahmad <ahmadHidayat@gmail.com>
2026-04-21 11:22:58 +0700

    update config

---
+++ apigw-pipeline/jte/pipeline_config.groovy
@@ -0,0 +1,25 @@
+libraries {
+  apigw {
+    apiProject = 'apiMsCustomer'
+
+    gatewayUrl = 'http://100.87.70.99:25155'
+
+    gatewayUrls = [
+    'http://100.87.70.99:25155',
+    'http://100.87.70.99:45155',
+    'http://100.88.89.10:25155',
+    'http://100.119.82.110:5555'
+  ]
+
+    esUrls = [
+    'http://100.87.70.99:29240',
+    'http://100.87.70.99:49240',
+    'http://100.88.89.10:29240',
+    'http://100.119.82.110:9240'
+  ]
+
+    username = 'Administrator'
+    password = 'manage'
+    credentialsId = 'apigw-creds'
+  }
+}
