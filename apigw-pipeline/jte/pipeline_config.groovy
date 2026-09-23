4aa9b09c631c769cb967ebf0d441ce333157a7a1
ahmadhidayatt <ahmadhidayatt@gmail.com>
2026-09-23 15:29:56 +0700

    Fix github credential ID

--- apigw-pipeline/jte/pipeline_config.groovy
+++ apigw-pipeline/jte/pipeline_config.groovy
@@ -2,7 +2,7 @@ libraries {
   apigw {
     apiProject = 'Training00'
     repoUrl     = 'https://github.com/ahmadhidayatt/sample-cicd-webmethods-apim-sample.git'
-    repoCredId  = 'github-token'
+    repoCredId  = 'github'
     repoBranch  = 'main'
     gatewayUrl = 'http://84.247.147.48:9072'
 
