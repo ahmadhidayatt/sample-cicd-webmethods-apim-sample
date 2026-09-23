5deb7bc93693a5a77edc37ee550e8c0cb2353dec
ahmadhidayatt <ahmadhidayatt@gmail.com>
2026-09-23 15:29:43 +0700

    Update pipeline_config

--- apigw-pipeline/jte/pipeline_config.groovy
+++ apigw-pipeline/jte/pipeline_config.groovy
@@ -1,25 +1,19 @@
 libraries {
   apigw {
-    apiProject = 'apiMsCustomer'
-    repoUrl     = 'https://github.com/ahmadhidayatt/asset-api-testcicd-v2.git'
-    repoCredId  = 'gitpwd'
+    apiProject = 'Training00'
+    repoUrl     = 'https://github.com/ahmadhidayatt/sample-cicd-webmethods-apim-sample.git'
+    repoCredId  = 'github-token'
     repoBranch  = 'main'
-    gatewayUrl = 'http://100.87.70.99:25155'
+    gatewayUrl = 'http://84.247.147.48:9072'
 
     gatewayUrls = [
-    'http://100.87.70.99:25155',
-    'http://100.87.70.99:45155',
-    'http://100.88.89.10:25155',
-    'http://100.119.82.110:5555'
-  ]
+      'http://84.247.147.48:9072'
+    ]
 
     esUrls = [
-    'http://100.87.70.99:29240',
-    'http://100.87.70.99:49240',
-    'http://100.88.89.10:29240',
-    'http://100.119.82.110:9240'
-  ]
+      'http://84.247.147.48:9240'
+    ]
 
-    credentialsId = 'apigw-creds'
+    credentialsId = 'apigw-admin'
   }
 }
